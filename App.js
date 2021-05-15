3cc83bac5027cc52522b73493e9b5c03adba56b4
--replace-all <hishammoonnakal@gmail.com>
2021-05-15 18:11:22 +0530

    upload4

--- App.js
+++ App.js
@@ -86,15 +86,19 @@ const App = () => {
         centerComponent={{text: 'Machine Test', style: {color: '#fff'}}}
         rightComponent={
           <View>
-            <Switch
-              trackColor={{false: '#767577', true: '#81b0ff'}}
-              thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-              ios_backgroundColor="#3e3e3e"
-              onValueChange={() => {
-                setIsEnabled(!isEnabled);
-              }}
-              value={isEnabled}
-            />
+            {!isLoading ? (
+              <Switch
+                trackColor={{false: '#767577', true: '#81b0ff'}}
+                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={() => {
+                  setIsEnabled(!isEnabled);
+                }}
+                value={isEnabled}
+              />
+            ) : (
+              <View />
+            )}
           </View>
         }
       />
